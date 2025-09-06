ae95a6232cabe5d3757710e4d14c90591e0a0d71
Lee Wei <skvopsn765@gmail.com>
2025-09-06 23:11:21 +0800

    Refactor the solution to integrate a common 'merchent' structure and update file paths for input, improving the code's modularity and consistency across problem solutions.

--- Problems/ITSA/202412/Problem3/solution.cpp
+++ Problems/ITSA/202412/Problem3/solution.cpp
@@ -1,32 +1,45 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+struct merchent
+{
+    int index;
+    int employee;
+    int manager;
+};
 
 int main()
 {
     // Local testing: read from file
 #ifdef LOCAL
-    freopen("Problems/ITSA/202412/Problem3/input.txt", "r", stdin);
+    freopen("Problems/ITSA/202412/Problem5/input.txt", "r", stdin);
 #endif
 
     // 你的程式碼
-    int interval;
-    cin >> interval;
-
-    string line;
-    cin.ignore();
-    getline(cin, line);
-
-    string newStr;
-    int index = 0;
-
-    while (index < line.size())
+    int n, k;
+    cin >> n >> k;
+    vector<merchent> v1(n);
+    for (int i=0; i<n; i++)
     {
-        newStr += line[index];
-        index += interval + 1;
+        v1[i].index = i;
+        cin >> v1[i].employee >> v1[i].manager;
     }
-    cout << newStr << '\n';
+
+    sort(v1.begin(), v1.end(), [](merchent a, merchent b)
+    {
+       return a.employee > b.employee;
+    });
+
+    vector<merchent> v2(v1.begin(), v1.begin() + k);
+    sort(v2.begin(), v2.end(), [](merchent a, merchent b)
+    {
+        return a.manager > b.manager;
+    });
+
+    cout << v2[0].index + 1 << "\n";
 
     return 0;
 }
--- Problems/ITSA/202412/Problem5/solution.cpp
+++ Problems/ITSA/202412/Problem5/solution.cpp
@@ -4,14 +4,42 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+struct merchent
+{
+    int index;
+    int employee;
+    int manager;
+};
+
+int main()
+{
     // Local testing: read from file
-    #ifdef LOCAL
+#ifdef LOCAL
     freopen("Problems/ITSA/202412/Problem5/input.txt", "r", stdin);
-    #endif
-    
+#endif
+
     // 你的程式碼
-    
-    
+    int n, k;
+    cin >> n >> k;
+    vector<merchent> v1;
+    for (int i=0; i<n; i++)
+    {
+        v1[i].index = i;
+        cin >> v1[i].employee >> v1[i].manager;
+    }
+
+    sort(v1.begin(), v1.end(), [](struct merchent a, struct merchent b)
+    {
+       return a.employee > b.employee;
+    });
+
+    vector<merchent> v2(v1.begin(), v1.begin() + 2);
+    sort(v2.begin(), v2.end(), [](merchent a, merchent b)
+    {
+        return a.manager > b.manager;
+    });
+
+    cout << v2[0].index << "\n";
+
     return 0;
 }

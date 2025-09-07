94cf3a691492ccd36741a64203c32b9b9663423c
Lee Wei <skvopsn765@gmail.com>
2025-09-07 15:23:32 +0800

    Refactor the main logic to correctly calculate the number of purchases made while simplifying the count and total tracking, ensuring clarity in the output of the program.

--- Problems/ITSA/202411/Problem2/solution.cpp
+++ Problems/ITSA/202411/Problem2/solution.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
     // Local testing: read from file
-    #ifdef LOCAL
+#ifdef LOCAL
     freopen("Problems/ITSA/202411/Problem2/input.txt", "r", stdin);
-    #endif
+#endif
 
     // 你的程式碼
     int n;
@@ -16,19 +16,18 @@ int main() {
         int target;
         cin >> target;
 
-        int sum;
+        int buy = 0;
         int count = 0;
-        int total = 0;
         while (count < target)
         {
-            total++;
-            if (total % 3 == 0)
+            count++;
+            buy++;
+            if (count % 3 == 0)
             {
                 count++;
-                total++;
             }
         }
-        cout << total << "\n";
+        cout << buy << "\n";
     }
 
     return 0;

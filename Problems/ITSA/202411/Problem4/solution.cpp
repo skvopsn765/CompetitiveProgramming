c775155480d8304161d41f86c45b961f29b34c59
Lee Wei <skvopsn765@gmail.com>
2025-09-07 17:44:34 +0800

    Refactors the solution logic in both problem files to improve clarity and correctness, updating variable initialization, fixing comparison operators, and enhancing readability by adding necessary includes and proper formatting.

--- Problems/ITSA/202411/Problem4/solution.cpp
+++ Problems/ITSA/202411/Problem4/solution.cpp
@@ -1,16 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-int main() {
+int main()
+{
     // Local testing: read from file
-    #ifdef LOCAL
+#ifdef LOCAL
     freopen("Problems/ITSA/202411/Problem4/input.txt", "r", stdin);
-    #endif
-    
+#endif
+
     // 你的程式碼
-    
-    
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string input;
+        cin >> input;
+        int N;
+        for (int j = 0; j < input.size(); j++)
+        {
+            N += (input[i] - '0');
+        }
+        vector<int> factor;
+        int S;
+        for (int j = 1; j < N; j++)
+        {
+            if (N % j == 0)
+            {
+                factor.push_back(j);
+                S += j;
+            }
+        }
+        if (S > N) cout << "-1" << "\n";
+        if (S = N) cout << "0" << "\n";
+        if (S < N) cout << N - S << "\n";
+    }
+
     return 0;
 }

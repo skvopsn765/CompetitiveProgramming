6cff04f5dd3953762d9a9b4a25662706145a21c4
Lee Wei <skvopsn765@gmail.com>
2025-09-07 14:26:42 +0800

    Add a link to the problem description and refactor the solution code to replace the `merchent` structure with an `item` structure, improving clarity and functionality in the main algorithm.

--- Problems/ITSA/202412/Problem5/solution.cpp
+++ Problems/ITSA/202412/Problem5/solution.cpp
@@ -4,13 +4,6 @@
 #include <algorithm>
 using namespace std;
 
-struct merchent
-{
-    int index;
-    int employee;
-    int manager;
-};
-
 int main()
 {
     // Local testing: read from file
@@ -18,28 +11,31 @@ int main()
     freopen("Problems/ITSA/202412/Problem5/input.txt", "r", stdin);
 #endif
 
-    // 你的程式碼
     int n, k;
     cin >> n >> k;
-    vector<merchent> v1;
-    for (int i=0; i<n; i++)
+    struct item
     {
-        v1[i].index = i;
-        cin >> v1[i].employee >> v1[i].manager;
+        int first;
+        int second;
+        int index;
+    };
+    vector<item> itemV(n);
+    for (int i = 0; i < n; i++)
+    {
+        itemV[i].index = i;
+        cin >> itemV[i].first >> itemV[i].second;
     }
-
-    sort(v1.begin(), v1.end(), [](struct merchent a, struct merchent b)
+    sort(itemV.begin(), itemV.end(), [](item a, item b)
     {
-       return a.employee > b.employee;
+        return a.first > b.first;
+    });
+    vector<item> itemV2(itemV.begin(), itemV.begin() + k);
+    sort(itemV2.begin(), itemV2.end(), [](item a, item b)
+    {
+        return a.second > b.second;
     });
 
-    vector<merchent> v2(v1.begin(), v1.begin() + 2);
-    sort(v2.begin(), v2.end(), [](merchent a, merchent b)
-    {
-        return a.manager > b.manager;
-    });
-
-    cout << v2[0].index << "\n";
+    cout << itemV2[0].index + 1 << "\n";
 
     return 0;
 }

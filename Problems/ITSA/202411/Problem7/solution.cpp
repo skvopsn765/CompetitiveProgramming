08f9232676d332f0c3eabddb7dfe1b5c2229c9ec
Lee Wei <skvopsn765@gmail.com>
2025-09-07 21:21:43 +0800

    Add problem description, input, expected output, and initial solution structure for Problem 7 of the ITSA competition, implementing a card game where players minimize point deductions

---
+++ Problems/ITSA/202411/Problem7/solution.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Local testing: read from file
+    #ifdef LOCAL
+    freopen("Problems/ITSA/202411/Problem7/input.txt", "r", stdin);
+    #endif
+    
+    // 你的程式碼
+    
+    
+    return 0;
+}

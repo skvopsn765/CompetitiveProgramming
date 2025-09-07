1af2732ec1d0ff4b793d33bb38a8be3a2b04177b
Lee Wei <skvopsn765@gmail.com>
2025-09-07 21:14:43 +0800

    Refactor input handling and logic in solution.cpp and main.cpp to manage grid-based bomb counting and error checking, ensuring proper reading of data and maintaining the structure of the program for competitive programming tasks.

--- Problems/ITSA/202411/Problem6/solution.cpp
+++ Problems/ITSA/202411/Problem6/solution.cpp
@@ -4,14 +4,59 @@
 #include <string>
 using namespace std;
 
-int main() {
+int main()
+{
     // Local testing: read from file
-    #ifdef LOCAL
+#ifdef LOCAL
     freopen("Problems/ITSA/202411/Problem6/input.txt", "r", stdin);
-    #endif
-    
+#endif
+
     // 你的程式碼
-    
-    
+    int n, m;
+    cin >> n >> m;
+    vector<string> row(m);
+    vector<vector<string>> area(n);
+    vector<pair<int, int>> numPos;
+    for (int i = 0; i < n; i++)
+    {
+        string rowStr;
+        cin >> rowStr;
+        for (int j = 0; j < m; j++)
+        {
+            area[i][j] = rowStr[j];
+            if (rowStr[j] >= '0' && rowStr[j] < 'A')
+            {
+                numPos.emplace_back(i, j);
+            }
+        }
+    }
+
+    // (a - 1, b - 1), (a - 1, b), (a - 1, b + 1)
+    // (a    , b - 1), (a    , b), (a    , b + 1)
+    // (a + 1, b - 1), (a + 1, b), (a + 1, b + 1)
+
+    for (auto pos : numPos)
+    {
+        int a = pos.first;
+        int b = pos.second;
+        int bombCount = 0;
+        for (int i = a - 1; i <= a + 1; i++)
+        {
+            if (i > n) continue;
+            for (int j = b - 1; j <= b + 1; j++)
+            {
+                if (j > m) continue;
+                if (area[i][j][0] == '*' || area[i][j][0] == '?')
+                {
+                    bombCount++;
+                }
+            }
+        }
+
+        if (bombCount != area[a][b][0] - '0') cout << "ERROR" << "\n";
+    }
+
+    cout << "PASS" << "\n";
+
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,41 +1,74 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
     // Local testing: read from file
 #ifdef LOCAL
-    freopen("Problems/ITSA/202411/Problem4/input.txt", "r", stdin);
+    freopen("Problems/ITSA/202411/Problem6/input.txt", "r", stdin);
 #endif
 
     // 你的程式碼
-    int n;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
+    vector<vector<string>> area(n, vector<string>(m));
+    vector<pair<int, int>> numPos;
     for (int i = 0; i < n; i++)
     {
-        string input;
-        cin >> input;
-        int N = 0;
-        for (char j : input)
+        string rowStr;
+        cin >> rowStr;
+        for (int j = 0; j < m; j++)
         {
-            N += (j - '0');
-        }
-        int S = 1;
-        for (int j = 2; j * j < N; j++)
-        {
-            if (N % j == 0)
+            area[i][j] = rowStr[j];
+            if (rowStr[j] >= '0' && rowStr[j] <= '9')
             {
-                S += j;
-                if (j != N / j) S += N / j;
+                numPos.emplace_back(i, j);
             }
         }
-        if (S > N) cout << "-1" << "\n";
-        if (S == N) cout << "0" << "\n";
-        if (S < N) cout << N - S << "\n";
     }
+
+    // (a - 1, b - 1), (a - 1, b), (a - 1, b + 1)
+    // (a    , b - 1), (a    , b), (a    , b + 1)
+    // (a + 1, b - 1), (a + 1, b), (a + 1, b + 1)
+
+    for (auto pos : numPos)
+    {
+        int a = pos.first;
+        int b = pos.second;
+        int bombCount = 0;
+        int questionCount = 0;
+        for (int i = a - 1; i <= a + 1; i++)
+        {
+            if (i > n - 1 || i < 0) continue;
+            for (int j = b - 1; j <= b + 1; j++)
+            {
+                if (j > m - 1 || j < 0) continue;
+                if (area[i][j][0] == '*')
+                {
+                    bombCount++;
+                }
+                if (area[i][j][0] == '?')
+                {
+                    questionCount++;
+                }
+            }
+        }
+
+        int markCount = area[a][b][0] - '0';
+        if (bombCount != markCount)
+        {
+            if (bombCount + questionCount < markCount)
+            {
+                cout << "ERROR" << "\n";
+                return 0;
+            }
+        }
+    }
+
+    cout << "PASS" << "\n";
 
     return 0;
 }

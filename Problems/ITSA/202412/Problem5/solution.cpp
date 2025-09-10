197e3a8151ec26f25fa4b291ffddea1ef2d9001d
Lee Wei <skvopsn765@gmail.com>
2025-09-10 15:20:05 +0800

    Add logic to read merchant data, sort by two criteria, and output the index of the top merchant based on the highest values, enhancing the solution's functionality for the given problem.

--- Problems/ITSA/202412/Problem5/solution.cpp
+++ Problems/ITSA/202412/Problem5/solution.cpp
@@ -11,7 +11,34 @@ int main()
     freopen("Problems/ITSA/202412/Problem5/input.txt", "r", stdin);
 #endif
 
+    int N, K;
+    cin >> N >> K;
+    struct merchent
+    {
+        int v1;
+        int v2;
+        int index;
+    };
+    vector<merchent> mv(N);
+    mv.reserve(N);
+    for (int i = 0; i < N; i++)
+    {
+        mv[i].index = i + 1;
+        cin >> mv[i].v1 >> mv[i].v2;
+    }
 
+    sort(mv.begin(), mv.end(), [](merchent a, merchent b)
+    {
+        return a.v1 > b.v1;
+    });
+
+    vector<merchent> mv2(mv.begin(), mv.begin() + K);
+    sort(mv2.begin(), mv2.end(), [](merchent a, merchent b)
+    {
+        return a.v2 > b.v2;
+    });
+
+    cout << mv2[0].index << "\n";
 
     return 0;
 }

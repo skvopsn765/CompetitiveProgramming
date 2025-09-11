52a387ff59bcfe313308888ed577a52da28054f8
Lee Wei <skvopsn765@gmail.com>
2025-09-12 01:09:04 +0800

    Implement the solution for Luogu P1706, generating all unique permutations of numbers from 1 to n, including the input and expected output files for testing.

--- Problems/Luogu/P1706-全排列问题/solution.cpp
+++ Problems/Luogu/P1706-全排列问题/solution.cpp
@@ -1,8 +1,10 @@
 // https://www.luogu.com.cn/problem/P1706
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
-
+#include <iostream>
 using namespace std;
+
 const int N = 1000;
 int n;
 int st[N];
@@ -17,11 +19,12 @@ void dfs(int x)
             printf("%5d", arr[i]);
         }
         printf("\n");
+        return;  // 修正：加上 return
     }
 
     for (int i = 1; i <= n; i++)
     {
-        if (!st[N])
+        if (!st[i])  // 修正：應該是 st[i] 不是 st[N]
         {
             st[i] = true;
             arr[x] = i;
@@ -34,6 +37,11 @@ void dfs(int x)
 
 int main()
 {
+#ifdef LOCAL
+    freopen("Problems/Luogu/P1706-全排列问题/input.txt", "r", stdin);
+    freopen("Problems/Luogu/P1706-全排列问题/output.txt", "w", stdout);
+#endif
+
     scanf("%d", &n);
     dfs(1);
     return 0;
--- main.cpp
+++ main.cpp
@@ -1,86 +1,47 @@
+// https://www.luogu.com.cn/problem/P1706
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
 #include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <cctype>
 using namespace std;
 
-// 常數定義（避免 Magic Number）
-const int MIN_VALUE = 0;
-const int MAX_VALUE = 1000000000; // 1e9
+const int N = 1000;
+int n;
+int st[N];
+int arr[N];
 
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-        int a,b,c;
-        a=0;b=0;c=0;
-        for (int i=0; i<nums.size(); i++)
-        {
-            a = max(b, c+nums[i]);
-            c = b;
-            b = a;
-        }
-
-        return a;
-    }
-};
-
-// 讀取一行並解析為整數陣列
-// 支援兩種格式：
-// 1) JSON 陣列，如: [2,7,9,3,1]
-// 2) 空白分隔，如: 2 7 9 3 1
-static vector<int> readIntArrayFlexible(istream& in)
+void dfs(int x)
 {
-    vector<int> nums;
-    string line;
-    if (!getline(in, line)) {
-        return nums;
-    }
-    // 去除 UTF-8 BOM（若存在）
-    if (line.size() >= 3 && (unsigned char)line[0] == 0xEF && (unsigned char)line[1] == 0xBB && (unsigned char)line[2] == 0xBF) {
-        line.erase(0, 3);
-    }
-    // 去除前後空白
-    auto notSpace = [](int ch) { return !isspace(ch); };
-    line.erase(line.begin(), find_if(line.begin(), line.end(), notSpace));
-    line.erase(find_if(line.rbegin(), line.rend(), notSpace).base(), line.end());
-    if (line.empty()) return nums;
-
-    if (line.find('[') != string::npos && line.find(']') != string::npos) {
-        string t;
-        t.reserve(line.size());
-        for (char c : line) {
-            if (c == '[' || c == ']' || c == ',') c = ' ';
-            t.push_back(c);
-        }
-        stringstream ss(t);
-        long long x;
-        while (ss >> x) nums.push_back((int)x);
-        return nums;
-    }
-
+    if (x > n)
     {
-        stringstream ss(line);
-        long long x;
-        while (ss >> x) nums.push_back((int)x);
+        for (int i = 1; i <= n; i++)
+        {
+            printf("%5d", arr[i]);
+        }
+        printf("\n");
+        return;  // 修正：加上 return
     }
-    return nums;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (!st[i])  // 修正：應該是 st[i] 不是 st[N]
+        {
+            st[i] = true;
+            arr[x] = i;
+            dfs(x + 1);
+            st[i] = false;
+            arr[x] = 0;
+        }
+    }
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 #ifdef LOCAL
-    freopen("Problems/LeetCode/0198-house-robber/input.txt", "r", stdin);
+    freopen("Problems/Luogu/P1706-全排列问题/input.txt", "r", stdin);
 #endif
 
-    const auto solution = new Solution();
-    vector<int> nums = readIntArrayFlexible(cin);
-    const int answer = solution->rob(nums);
-    cout << answer << "\n";
+    scanf("%d", &n);
+    dfs(1);
     return 0;
 }

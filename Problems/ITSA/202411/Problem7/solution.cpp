66e1d9a3d04d0570c86aa7169e1b9b6b5d069b31
Lee Wei <skvopsn765@gmail.com>
2025-09-12 00:57:44 +0800

    Remove unused stack include in the solution.cpp file and add a new solution.cpp file for the permutation problem, implementing a depth-first search algorithm for generating permutations of a given integer n.

--- Problems/ITSA/202411/Problem7/solution.cpp
+++ Problems/ITSA/202411/Problem7/solution.cpp
@@ -1,6 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <stack>
 #include <vector>
 using namespace std;
 
---
+++ Problems/Others/全排列问题/solution.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+
+using namespace std;
+const int N = 1000;
+int n;
+int arr[N];
+bool st[N];
+
+void dfs(int x) {
+    if (x > n) {
+        for (int i = 1; i <= n; i++) {
+            printf("%5d", arr[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (!st[i]) {
+            st[i] = true;
+            arr[x] = i;
+            dfs(x + 1);
+            st[i] = false;
+            arr[x] = 0;
+        }
+    }
+}
+
+int main() {
+    scanf("%d", &n);
+    dfs(1);
+    return 0;
+}

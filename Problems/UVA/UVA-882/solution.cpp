0b414de64ed3dc45b7759c3aaf66bdce00abc0bc
Lee Wei <skvopsn765@gmail.com>
2025-09-19 21:43:09 +0800

    Refactor the solution for UVA 882 by improving the DFS implementation and enhancing readability, while also updating the input handling to accommodate multiple test cases efficiently.

--- Problems/UVA/UVA-882/solution.cpp
+++ Problems/UVA/UVA-882/solution.cpp
@@ -1,45 +1,52 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
-#include <cstring>
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
 #include <algorithm>
+
 using namespace std;
+#define maxk 10+5
+#define maxm 100+5
+#define INF 0x3f3f3f3f
 
-const int MAX_K = 15;
-const int MAX_M = 105;
-const int INF_COST = 0x3f3f3f3f;
+int dp[maxk][maxm][maxm];
+bool visited[maxk][maxm][maxm];
 
-static int arrMemo[MAX_K][MAX_M][MAX_M];
-static unsigned char visitedMemo[MAX_K][MAX_M][MAX_M];
+int dfs(int k, int i, int j)
+{
+    if (i > j)
+        return 0;
 
-static int tri(int n) { return n * (n + 1) / 2; }
+    if (k == 0)
+        return INF;
 
-int solveDP(int k, int i, int j) {
-    if (i > j) return 0;
-    if (i == j) return i;
-    if (k == 1) return tri(j) - tri(i - 1);
-    if (visitedMemo[k][i][j]) return arrMemo[k][i][j];
-    int best = INF_COST;
-    for (int x = i; x <= j; x++) {
-        best = min(best, x + max(solveDP(k - 1, i, x - 1), solveDP(k, x + 1, j)));
-    }
-    visitedMemo[k][i][j] = 1;
-    arrMemo[k][i][j] = best;
-    return best;
+    if (visited[k][i][j])
+        return dp[k][i][j];
+
+    visited[k][i][j] = true;
+    int& ret = dp[k][i][j];
+
+    int x;
+    ret = INF;
+
+    for (x = i; x <= j; x++)
+        ret = min(ret, x + max(dfs(k - 1, i, x - 1), dfs(k, x + 1, j)));
+
+    return ret;
 }
 
-int main() {
+int main()
+{
 #ifdef LOCAL
     freopen("Problems/UVA/UVA-882/input.txt", "r", stdin);
 #endif
-    int T;
-    if (scanf("%d", &T) != 1) return 0;
-    while (T--) {
-        int k, m;
+    int Cas, k, m;
+    memset(visited, false, sizeof(visited));
+    scanf("%d", &Cas);
+
+    while (Cas--)
+    {
         scanf("%d %d", &k, &m);
-        memset(visitedMemo, 0, sizeof(visitedMemo));
-        int ans = solveDP(k, 1, m);
-        printf("%d\n", ans);
+        printf("%d\n", dfs(k, 1, m));
     }
     return 0;
 }
-
--- main.cpp
+++ main.cpp
@@ -4,42 +4,42 @@
 #include <algorithm>
 using namespace std;
 
-const int MAX_K = 15;
-const int MAX_M = 105;
-const int INF_COST = 0x3f3f3f3f;
+const int max_k = 10;
+const int max_m = 100;
+int arr[max_k][max_m];
+const int INF = 0x3f3f3f3f;
 
-static int arrMemo[MAX_K][MAX_M][MAX_M];
-static unsigned char visitedMemo[MAX_K][MAX_M][MAX_M];
-
-static int tri(int n) { return n * (n + 1) / 2; }
-
-int solveDP(int k, int i, int j) {
-    if (i > j) return 0;
-    if (i == j) return i;
-    if (k == 1) return tri(j) - tri(i - 1);
-    if (visitedMemo[k][i][j]) return arrMemo[k][i][j];
-    int best = INF_COST;
-    for (int x = i; x <= j; x++) {
-        best = min(best, x + max(solveDP(k - 1, i, x - 1), solveDP(k, x + 1, j)));
+int dfs(int k, int m)
+{
+    if (k == 0 || m == 0) return 0;
+    if (k == 1) return m * (m + 1) / 2;
+    if (arr[k][m] != 0) return arr[k][m];
+    int min = INF;
+    for (int x = 1; x <= m; x++)
+    {
+        int value = x + max(dfs(k-1, x-1), dfs(k, m-x));
+        if (value < min) min = value;
     }
-    visitedMemo[k][i][j] = 1;
-    arrMemo[k][i][j] = best;
-    return best;
+    arr[k][m] = min;
+
+    return min;
 }
 
-int main() {
+int main()
+{
 #ifdef LOCAL
     freopen("Problems/UVA/UVA-882/input.txt", "r", stdin);
 #endif
     int T;
-    if (scanf("%d", &T) != 1) return 0;
-    while (T--) {
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++)
+    {
         int k, m;
-        scanf("%d %d", &k, &m);
-        memset(visitedMemo, 0, sizeof(visitedMemo));
-        int ans = solveDP(k, 1, m);
+        scanf("%d", &k);
+        scanf("%d", &m);
+        int ans = dfs(k, m);
         printf("%d\n", ans);
     }
+
     return 0;
 }
-

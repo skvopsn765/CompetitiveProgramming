8c2b7c6f9f567de2e3da1cecf5af00f203c6666c
Lee Wei <skvopsn765@gmail.com>
2025-09-21 17:57:39 +0800

    Update input and expected output formats in problem descriptions, and clean up the solution code structure for UVA 11087 and ITSA 202412.

--- Problems/UVA/UVA-11087/solution.cpp
+++ Problems/UVA/UVA-11087/solution.cpp
@@ -3,46 +3,42 @@
 #include <cstdio>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
-int main() {
+// const int MAX_N = 100001;
+// int arr[MAX_N];
+
+
+int dp(int x)
+{
+
+}
+
+int main()
+{
 #ifdef LOCAL
     freopen("Problems/UVA/UVA-11087/input.txt", "r", stdin);
 #endif
 
     int T;
     if (scanf("%d", &T) != 1) return 0;
-    for (int t = 1; t <= T; t++) {
+    for (int t = 1; t <= T; t++)
+    {
         int n, k;
         scanf("%d %d", &n, &k);
-        unordered_map<int,int> freq;
-        freq.reserve((size_t)n * 2u);
-        for (int i = 0; i < n; i++) {
-            int x; scanf("%d", &x);
-            freq[x]++;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", arr[i]);
         }
-        vector<vector<int>> groups(k);
-        vector<int> dupCount(k, 0);
-        groups.shrink_to_fit();
-        for (auto &p : freq) {
-            int v = p.first;
-            int c = p.second;
-            int r = ((v % k) + k) % k;
-            groups[r].push_back(v);
-            if (c >= 2) dupCount[r]++;
-        }
-        long long ans = 0;
-        for (int r = 0; r < k; r++) {
-            int tr = (k - r) % k;
-            if (r < tr) {
-                ans += 1LL * groups[r].size() * groups[tr].size();
-            } else if (r == tr) {
-                long long s = (long long)groups[r].size();
-                ans += s * (s - 1) / 2;
-                ans += dupCount[r];
-            }
-        }
-        printf("Case %d: %lld\n", t, ans);
+        sort(arr.begin(), arr.end(), [](int a, int b)
+        {
+            return a < b;
+        });
+
+
+
     }
     return 0;
 }

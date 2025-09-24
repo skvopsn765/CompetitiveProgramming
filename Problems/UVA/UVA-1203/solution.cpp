49ab526bea40beb0b3e0c6deadbd49e3cad7f2fa
Lee Wei <skvopsn765@gmail.com>
2025-09-24 11:21:15 +0800

    Add UVA-1203 problem files, including input, expected output, problem description, and solution, while also organizing project structure by creating a new folder for UVA problems.

---
+++ Problems/UVA/UVA-1203/solution.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+#ifdef LOCAL
+    // 本地測試：從檔案讀取輸入（如需輸出到檔案可解除下行註解）
+    freopen("Problems/UVA/UVA-1203/input.txt", "r", stdin);
+#endif
+
+
+    return 0;
+}
+
+
